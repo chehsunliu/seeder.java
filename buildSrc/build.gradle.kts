c0a6e4c0f682fa50781913dcd611d3b77a20b707
chehsunliu <chehsunliu@gmail.com>
2025-07-15 01:12:51 +0800

    Add settings for publishing

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,4 +8,5 @@ repositories {
 
 dependencies {
     implementation("com.diffplug.spotless:com.diffplug.spotless.gradle.plugin:7.1.0")
+    implementation("com.vanniktech.maven.publish:com.vanniktech.maven.publish.gradle.plugin:0.34.0")
 }
